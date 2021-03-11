3e8e277338292255774fbcb650df32190f9d61f4
JieamI <1732615826@qq.com>
2021-03-11 16:30:13 +0800

    change config

--- frontend/src/config/basic.js
+++ frontend/src/config/basic.js
@@ -1,8 +1,8 @@
 const Config = {
 	//前端的域名（必须是域名，不能是ip:port形式），注意域名最后不要加斜杠
-	FrontDomain: process.env.VUE_APP_FRONT_DOMAIN ? process.env.VUE_APP_FRONT_DOMAIN : "https://join.itoken.team",
+	FrontDomain: process.env.VUE_APP_FRONT_DOMAIN ? process.env.VUE_APP_FRONT_DOMAIN : "https://recruit.itoken.team",
 	//后端域名（可以是ip:port形式），注意最后不要加斜杠
-	BackendDomain: process.env.VUE_APP_BACKEND_DOMAIN ? process.env.VUE_APP_BACKEND_DOMAIN : "http:localhost:8000/api",
+	BackendDomain: process.env.VUE_APP_BACKEND_DOMAIN ? process.env.VUE_APP_BACKEND_DOMAIN : "https://recruit.itoken.team/api",
 	// //钉钉AppId
 	DingAppId: "dingoalrnidom4p3khixr0",
 	// //钉钉AppSecret
